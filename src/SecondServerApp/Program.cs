13218a3dcfc076cb1c2af67a7100e3f906df0330
TSrgy <20205309+TSrgy@users.noreply.github.com>
2021-12-12 09:43:03 +0500

    Add a second server app

---
+++ src/SecondServerApp/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Map("/second", mappedApp =>
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        mappedApp.UseSwagger();
+        mappedApp.UseSwaggerUI();
+    }
+
+    mappedApp.UseAuthorization();
+
+    mappedApp.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+});
+
+
+app.Run();
+
+
